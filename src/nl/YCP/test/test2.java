fe8d437daa238abcf905459a19669adf167f89c0
Jelle vd Veen <jellevdveen16@gmail.com>
2016-03-15 11:32:09 +0100

    update test hex value to byte

--- src/nl/YCP/test/test2.java
+++ src/nl/YCP/test/test2.java
@@ -3,8 +3,9 @@ package nl.YCP.test;
 public class test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	byte b;
+	b = (byte)0xFB;
+	System.out.println(b);
 	}
 
 }
