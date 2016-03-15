2fd58509e32154e80acb0c9bed8a6dc38c19ffa7
Jelle vd Veen <jellevdveen16@gmail.com>
2016-03-15 16:46:15 +0100

    made file importer

--- src/nl/YCP/test/test2.java
+++ src/nl/YCP/test/test2.java
@@ -1,15 +1,41 @@
 package nl.YCP.test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class test2 {
 
 	public static void main(String[] args) {
-	float b;
-	int a = 100;
-	System.out.println(b = a);
-	
-	ArrayList<ArrayList<Integer>> ar = new ArrayList<>();
+		int[][] numbers;
+		try {
+			Scanner s = new Scanner(new File("Array.txt"));
+			ArrayList<String[]> stringNumbers = new ArrayList<>();
+			while (s.hasNext()) {
+				stringNumbers.add(0, s.nextLine().split(" "));
+			}
+			numbers = new int[stringNumbers.size()][];
+			for (int i = 0; i < numbers.length; i++) {
+				numbers[i] = new int[stringNumbers.get(i).length];
+				for (int j = 0; j < stringNumbers.get(i).length; j++) {
+					numbers[i][j] = Integer.valueOf(stringNumbers.get(i)[j]);
+				}
+			}
+			for (int i = 0; i < numbers.length; i++) {
+				for (int j = 0; j < numbers[i].length; j++){
+					System.out.println(numbers[i][j]);
+				}
+			}
+				
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		} 
+		
+		
+		
+		
+		
+		
 	}
-
 }
